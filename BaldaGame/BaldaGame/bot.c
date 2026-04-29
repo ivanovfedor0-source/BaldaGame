a6041ee0f77c54e716b89e82802dd44b498906e7
Feseris <fev0n0v@gmail.com>
2026-04-29 22:13:55 +0300

    обновил ботов с новым бфс

--- BaldaGame/BaldaGame/bot.c
+++ BaldaGame/BaldaGame/bot.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#pragma comment(linker, "/STACK:16777216")
 #include "bot.h"
 #include "game.h"
 #include "dictionary.h"
@@ -19,20 +20,11 @@ typedef struct {
 static const unsigned char allRussianLetters[] = {
     0xC0, 0xC1, 0xC2, 0xC3, 0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xCB, 0xCC, 0xCD, 0xCE, 0xCF,
     0xD0, 0xD1, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xDB, 0xDC, 0xDD, 0xDE, 0xDF,
-    0xA8 
+    0xA8
 };
 
 static const unsigned char frequentLetters[] = {
-    0xC0,  // А
-    0xC5,  // Е
-    0xC8,  // И
-    0xCE,  // О
-    0xD3,  // У
-    0xD2,  // Т
-    0xCD,  // Н
-    0xD1,  // С
-    0xD0,  // Р
-    0xC2   // В
+    0xC0, 0xC5, 0xC8, 0xCE, 0xD3, 0xD2, 0xCD, 0xD1, 0xD0, 0xC2
 };
 
 #define RUSSIAN_LETTERS_COUNT 33
@@ -64,15 +56,11 @@ typedef struct {
 int hasAdjacentLetter(int row, int col) {
     int dr[] = { -1, 1, 0, 0 };
     int dc[] = { 0, 0, -1, 1 };
-
     for (int i = 0; i < 4; i++) {
         int newRow = row + dr[i];
         int newCol = col + dc[i];
-
         if (newRow >= 0 && newRow < BOARD_SIZE && newCol >= 0 && newCol < BOARD_SIZE) {
-            if (board[newRow][newCol].letter != 0) {
-                return 1;
-            }
+            if (board[newRow][newCol].letter != 0) return 1;
         }
     }
     return 0;
@@ -91,9 +79,7 @@ int getCachedPoints(int row, int col, unsigned char letter, int player, char* wo
     for (int i = 0; i < cacheSize; i++) {
         if (moveCache[i].row == row && moveCache[i].col == col &&
             moveCache[i].letter == letter && moveCache[i].player == player) {
-            if (word != NULL) {
-                strcpy(word, moveCache[i].word);
-            }
+            if (word) strcpy(word, moveCache[i].word);
             return moveCache[i].points;
         }
     }
@@ -117,10 +103,22 @@ void clearCache() {
     cacheSize = 0;
 }
 
+void saveGameState(int* savedPlayerScore, int* savedBotScore, int* savedUsedCount, char savedUsedWords[1000][30]) {
+    *savedPlayerScore = playerScore;
+    *savedBotScore = botScore;
+    *savedUsedCount = getUsedWordsCount();
+    copyUsedWords(savedUsedWords, savedUsedCount);
+}
+
+void restoreGameState(int savedPlayerScore, int savedBotScore, int savedUsedCount, char savedUsedWords[1000][30]) {
+    playerScore = savedPlayerScore;
+    botScore = savedBotScore;
+    restoreUsedWords(savedUsedWords, savedUsedCount);
+}
+
 void placeRandomLetter() {
     int emptyCells[25][2];
     int emptyCount = 0;
-
     for (int i = 0; i < BOARD_SIZE; i++) {
         for (int j = 0; j < BOARD_SIZE; j++) {
             if (board[i][j].letter == 0 && hasAdjacentLetter(i, j)) {
@@ -130,7 +128,6 @@ void placeRandomLetter() {
             }
         }
     }
-
     if (emptyCount > 0) {
         srand((unsigned int)time(NULL));
         int cellIdx = rand() % emptyCount;
@@ -138,9 +135,7 @@ void placeRandomLetter() {
         int row = emptyCells[cellIdx][0];
         int col = emptyCells[cellIdx][1];
         unsigned char letter = allRussianLetters[letterIdx];
-
-        printf("Bot places random letter '%c' at (%d,%d) (no winning moves)\n",
-            letter, row + 1, col + 1);
+        printf("Bot places random letter '%c' at (%d,%d)\n", letter, row + 1, col + 1);
         placeRussianLetter(row, col, letter);
         checkAndAddWordDirect(row, col, 2);
     }
@@ -151,13 +146,10 @@ void placeRandomLetter() {
 
 int evaluateMove(int row, int col, unsigned char letter, int player, char* bestWord) {
     int cached = getCachedPoints(row, col, letter, player, bestWord);
-    if (cached >= 0) {
-        return cached;
-    }
+    if (cached >= 0) return cached;
 
-    unsigned char originalLetter = board[row][col].letter;
-    int originalOwner = board[row][col].owner;
-
+    unsigned char oldLetter = board[row][col].letter;
+    int oldOwner = board[row][col].owner;
     board[row][col].letter = letter;
     board[row][col].owner = player;
 
@@ -169,7 +161,6 @@ int evaluateMove(int row, int col, unsigned char letter, int player, char* bestW
 
     BFSStateEval queue[MAX_QUEUE_EVAL];
     int front = 0, rear = 0;
-
     queue[rear].row = row;
     queue[rear].col = col;
     queue[rear].word[0] = board[row][col].letter;
@@ -179,52 +170,45 @@ int evaluateMove(int row, int col, unsigned char letter, int player, char* bestW
     queue[rear].visited[row][col] = 1;
     rear++;
 
+    int dr[] = { -1, 1, 0, 0 };
+    int dc[] = { 0, 0, -1, 1 };
+
     while (front < rear) {
-        BFSStateEval current = queue[front];
-        front++;
-
-        if (current.len >= 3 && current.len <= 8) {
-            char wordCopy[30];
-            strcpy(wordCopy, current.word);
-
-            if (isWordInDictionary(wordCopy)) {
-                if (!isWordUsedGlobal(wordCopy) && current.len > bestPoints) {
-                    bestPoints = current.len;
-                    strcpy(tempBestWord, wordCopy);
+        BFSStateEval curr = queue[front++];
+        if (curr.len >= 3 && curr.len <= 8) {
+            if (isWordInDictionary(curr.word) && !isWordUsedGlobal(curr.word)) {
+                if (curr.len > bestPoints) {
+                    bestPoints = curr.len;
+                    strcpy(tempBestWord, curr.word);
                 }
             }
-
-            char reversed[30];
-            strcpy(reversed, wordCopy);
-            reverseStringLocal(reversed);
-            if (isWordInDictionary(reversed)) {
-                if (!isWordUsedGlobal(reversed) && current.len > bestPoints) {
-                    bestPoints = current.len;
-                    strcpy(tempBestWord, reversed);
+            else {
+                char rev[30];
+                strcpy(rev, curr.word);
+                reverseStringLocal(rev);
+                if (isWordInDictionary(rev) && !isWordUsedGlobal(rev)) {
+                    if (curr.len > bestPoints) {
+                        bestPoints = curr.len;
+                        strcpy(tempBestWord, rev);
+                    }
                 }
             }
         }
-
-        if (current.len >= 8) continue;
-
-        int dr[] = { -1, 1, 0, 0 };
-        int dc[] = { 0, 0, -1, 1 };
-
+        if (curr.len >= 8) continue;
         for (int i = 0; i < 4; i++) {
-            int newRow = current.row + dr[i];
-            int newCol = current.col + dc[i];
-
-            if (newRow >= 0 && newRow < BOARD_SIZE && newCol >= 0 && newCol < BOARD_SIZE) {
-                if (board[newRow][newCol].letter != 0 && !current.visited[newRow][newCol]) {
+            int nr = curr.row + dr[i];
+            int nc = curr.col + dc[i];
+            if (nr >= 0 && nr < BOARD_SIZE && nc >= 0 && nc < BOARD_SIZE) {
+                if (board[nr][nc].letter != 0 && !curr.visited[nr][nc]) {
                     if (rear < MAX_QUEUE_EVAL) {
-                        queue[rear].row = newRow;
-                        queue[rear].col = newCol;
-                        strcpy(queue[rear].word, current.word);
-                        queue[rear].word[current.len] = board[newRow][newCol].letter;
-                        queue[rear].word[current.len + 1] = '\0';
-                        queue[rear].len = current.len + 1;
-                        memcpy(queue[rear].visited, current.visited, sizeof(current.visited));
-                        queue[rear].visited[newRow][newCol] = 1;
+                        queue[rear].row = nr;
+                        queue[rear].col = nc;
+                        strcpy(queue[rear].word, curr.word);
+                        queue[rear].word[curr.len] = board[nr][nc].letter;
+                        queue[rear].word[curr.len + 1] = '\0';
+                        queue[rear].len = curr.len + 1;
+                        memcpy(queue[rear].visited, curr.visited, sizeof(curr.visited));
+                        queue[rear].visited[nr][nc] = 1;
                         rear++;
                     }
                 }
@@ -232,17 +216,13 @@ int evaluateMove(int row, int col, unsigned char letter, int player, char* bestW
         }
     }
 
-    board[row][col].letter = originalLetter;
-    board[row][col].owner = originalOwner;
+    board[row][col].letter = oldLetter;
+    board[row][col].owner = oldOwner;
     playerScore = savedPlayerScore;
     botScore = savedBotScore;
 
     addToCache(row, col, letter, player, bestPoints, tempBestWord);
-
-    if (bestWord != NULL && bestPoints > 0) {
-        strcpy(bestWord, tempBestWord);
-    }
-
+    if (bestWord && bestPoints > 0) strcpy(bestWord, tempBestWord);
     return bestPoints;
 }
 
@@ -250,7 +230,6 @@ int findAllMoves(BotMove* moves, int maxMoves, int useAllLetters) {
     int moveCount = 0;
     const unsigned char* letters = useAllLetters ? allRussianLetters : frequentLetters;
     int letterCount = useAllLetters ? RUSSIAN_LETTERS_COUNT : FREQUENT_LETTERS_COUNT;
-
     for (int i = 0; i < BOARD_SIZE && moveCount < maxMoves; i++) {
         for (int j = 0; j < BOARD_SIZE && moveCount < maxMoves; j++) {
             if (board[i][j].letter == 0 && hasAdjacentLetter(i, j)) {
@@ -258,7 +237,6 @@ int findAllMoves(BotMove* moves, int maxMoves, int useAllLetters) {
                     unsigned char letter = letters[k];
                     char bestWord[30] = "";
                     int points = evaluateMove(i, j, letter, 2, bestWord);
-
                     if (points > 0) {
                         moves[moveCount].row = i;
                         moves[moveCount].col = j;
@@ -271,17 +249,14 @@ int findAllMoves(BotMove* moves, int maxMoves, int useAllLetters) {
             }
         }
     }
-
     return moveCount;
 }
 
 void botMakeMoveEasy() {
     BotMove winningMoves[500];
     int winCount = findAllMoves(winningMoves, 500, 1);
-
     srand((unsigned int)time(NULL));
     int r = rand() % 100;
-
     if (winCount > 0 && r < 30) {
         int idx = rand() % winCount;
         BotMove m = winningMoves[idx];
@@ -299,9 +274,6 @@ void botMakeMoveMedium() {
     BotMove bestMove;
     bestMove.points = -1;
     bestMove.row = -1;
-    bestMove.col = -1;
-    bestMove.letter = 0;
-
     for (int i = 0; i < BOARD_SIZE; i++) {
         for (int j = 0; j < BOARD_SIZE; j++) {
             if (board[i][j].letter == 0 && hasAdjacentLetter(i, j)) {
@@ -309,8 +281,7 @@ void botMakeMoveMedium() {
                     unsigned char letter = allRussianLetters[k];
                     char bestWord[30] = "";
                     int points = evaluateMove(i, j, letter, 2, bestWord);
-
-                    if (points > 0 && points > bestMove.points) {
+                    if (points > bestMove.points) {
                         bestMove.points = points;
                         bestMove.row = i;
                         bestMove.col = j;
@@ -321,7 +292,6 @@ void botMakeMoveMedium() {
             }
         }
     }
-
     if (bestMove.row != -1) {
         printf("Bot (Medium) places '%c' at (%d,%d) for %d points (word: %s)\n",
             bestMove.letter, bestMove.row + 1, bestMove.col + 1, bestMove.points, bestMove.word);
@@ -335,7 +305,6 @@ void botMakeMoveMedium() {
                     for (int k = 0; k < RUSSIAN_LETTERS_COUNT; k++) {
                         unsigned char letter = allRussianLetters[k];
                         int points = evaluateMove(i, j, letter, 2, NULL);
-
                         if (points > bestMove.points) {
                             bestMove.points = points;
                             bestMove.row = i;
@@ -346,7 +315,6 @@ void botMakeMoveMedium() {
                 }
             }
         }
-
         if (bestMove.row != -1) {
             printf("Bot (Medium) places '%c' at (%d,%d) (no winning moves, %d points)\n",
                 bestMove.letter, bestMove.row + 1, bestMove.col + 1, bestMove.points);
@@ -359,91 +327,125 @@ void botMakeMoveMedium() {
     }
 }
 
-void botMakeMoveHard() {
-    BotMove bestMove;
-    bestMove.points = -999999;
-    bestMove.row = -1;
+// Оценка хода для Hard бота (без изменения глобального состояния)
+int evaluateMoveHard(int row, int col, unsigned char letter, int player) {
+    unsigned char oldLetter = board[row][col].letter;
+    int oldOwner = board[row][col].owner;
 
-    BotMove botMoves[500];
-    int botMoveCount = findAllMoves(botMoves, 500, 1);
+    board[row][col].letter = letter;
+    board[row][col].owner = player;
 
-    if (botMoveCount == 0) {
-        placeRandomLetter();
-        return;
-    }
+    int bestPoints = 0;
 
-    for (int i = 0; i < botMoveCount - 1; i++) {
-        for (int j = i + 1; j < botMoveCount; j++) {
-            if (botMoves[i].points < botMoves[j].points) {
-                BotMove temp = botMoves[i];
-                botMoves[i] = botMoves[j];
-                botMoves[j] = temp;
-            }
-        }
-    }
+    // Запускаем BFS от ВСЕХ клеток с буквами
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            if (board[i][j].letter == 0) continue;
 
-    int topMoves = botMoveCount < 10 ? botMoveCount : 10;
-    printf("Hard bot: evaluating top %d moves (total %d possible)\n", topMoves, botMoveCount);
+            BFSStateEval queue[5000];
+            int front = 0, rear = 0;
 
-    for (int m = 0; m < topMoves; m++) {
-        BotMove move = botMoves[m];
+            queue[rear].row = i;
+            queue[rear].col = j;
+            queue[rear].word[0] = board[i][j].letter;
+            queue[rear].word[1] = '\0';
+            queue[rear].len = 1;
+            memset(queue[rear].visited, 0, sizeof(queue[rear].visited));
+            queue[rear].visited[i][j] = 1;
+            rear++;
 
-        if (move.points >= 10) {
-            bestMove = move;
-            break;
-        }
+            int dr[] = { -1, 1, 0, 0 };
+            int dc[] = { 0, 0, -1, 1 };
 
-        unsigned char originalLetter = board[move.row][move.col].letter;
-        int originalOwner = board[move.row][move.col].owner;
-        board[move.row][move.col].letter = move.letter;
-        board[move.row][move.col].owner = 2;
+            while (front < rear) {
+                BFSStateEval curr = queue[front++];
 
-        int bestPlayerResponse = 0;
-        int checked = 0;
-
-        for (int i = 0; i < BOARD_SIZE && checked < 50; i++) {
-            for (int j = 0; j < BOARD_SIZE && checked < 50; j++) {
-                if (board[i][j].letter == 0 && hasAdjacentLetter(i, j)) {
-                    for (int k = 0; k < FREQUENT_LETTERS_COUNT && checked < 50; k++) {
-                        unsigned char letter = frequentLetters[k];
-                        int points = evaluateMove(i, j, letter, 1, NULL);
-                        if (points > bestPlayerResponse) {
-                            bestPlayerResponse = points;
+                if (curr.len >= 3 && curr.len <= 8) {
+                    // Проверяем, содержит ли слово НОВУЮ клетку
+                    if (curr.visited[row][col]) {
+                        if (isWordInDictionary(curr.word) && !isWordUsedGlobal(curr.word)) {
+                            if (curr.len > bestPoints) bestPoints = curr.len;
                         }
-                        checked++;
+                        else {
+                            char rev[30];
+                            strcpy(rev, curr.word);
+                            reverseStringLocal(rev);
+                            if (isWordInDictionary(rev) && !isWordUsedGlobal(rev)) {
+                                if (curr.len > bestPoints) bestPoints = curr.len;
+                            }
+                        }
+                    }
+                }
+
+                if (curr.len >= 8) continue;
+
+                for (int d = 0; d < 4; d++) {
+                    int nr = curr.row + dr[d];
+                    int nc = curr.col + dc[d];
+                    if (nr >= 0 && nr < BOARD_SIZE && nc >= 0 && nc < BOARD_SIZE) {
+                        if (board[nr][nc].letter != 0 && !curr.visited[nr][nc]) {
+                            if (rear < 4999) {
+                                queue[rear].row = nr;
+                                queue[rear].col = nc;
+                                strcpy(queue[rear].word, curr.word);
+                                queue[rear].word[curr.len] = board[nr][nc].letter;
+                                queue[rear].word[curr.len + 1] = '\0';
+                                queue[rear].len = curr.len + 1;
+                                memcpy(queue[rear].visited, curr.visited, sizeof(curr.visited));
+                                queue[rear].visited[nr][nc] = 1;
+                                rear++;
+                            }
+                        }
                     }
                 }
             }
         }
+    }
 
-        board[move.row][move.col].letter = originalLetter;
-        board[move.row][move.col].owner = originalOwner;
+    board[row][col].letter = oldLetter;
+    board[row][col].owner = oldOwner;
 
-        int score = move.points - bestPlayerResponse;
+    return bestPoints;
+}
 
-        if (score > bestMove.points) {
-            bestMove.points = score;
-            bestMove.row = move.row;
-            bestMove.col = move.col;
-            bestMove.letter = move.letter;
-            strcpy(bestMove.word, move.word);
+void botMakeMoveHard() {
+    BotMove bestMove;
+    bestMove.points = -1;
+    bestMove.row = -1;
+    bestMove.col = -1;
+    bestMove.letter = 0;
+
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        for (int j = 0; j < BOARD_SIZE; j++) {
+            if (board[i][j].letter == 0 && hasAdjacentLetter(i, j)) {
+                for (int k = 0; k < RUSSIAN_LETTERS_COUNT; k++) {
+                    unsigned char letter = allRussianLetters[k];
+                    int points = evaluateMoveHard(i, j, letter, 2);
+
+                    if (points > bestMove.points) {
+                        bestMove.points = points;
+                        bestMove.row = i;
+                        bestMove.col = j;
+                        bestMove.letter = letter;
+                    }
+                }
+            }
         }
     }
 
     if (bestMove.row != -1) {
-        printf("Bot (Hard) places '%c' at (%d,%d) (score: %d, word: %s)\n",
-            bestMove.letter, bestMove.row + 1, bestMove.col + 1, bestMove.points, bestMove.word);
+        printf("Bot (Hard) places '%c' at (%d,%d) for %d points\n",
+            bestMove.letter, bestMove.row + 1, bestMove.col + 1, bestMove.points);
         placeRussianLetter(bestMove.row, bestMove.col, bestMove.letter);
         checkAndAddWordDirect(bestMove.row, bestMove.col, 2);
     }
     else {
-        botMakeMoveMedium();
+        printf("Bot (Hard) has no valid moves!\n");
     }
 }
 
 void botMakeMove() {
     extern enum BotDifficulty botDifficulty;
-
     switch (botDifficulty) {
     case DIFFICULTY_EASY:
         botMakeMoveEasy();
